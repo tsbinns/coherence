c3800d231d60fde6c088047329150d6ba9b5ba6b
tsbinns <t.s.binns@outlook.com>
2021-12-29 23:33:58 +0100

    refactoring

--- coherence/coh_rereference.py
+++ coherence/coh_rereference.py
@@ -14,16 +14,20 @@ class Reref(ABC):
         raw
         ) -> tuple[np.array, mne.Info, list]:
         
-        return raw.get_data(reject_by_annotation='omit').copy(), raw.info.copy(), raw._get_channel_positions().copy().tolist()
+        return raw.get_data(reject_by_annotation='omit').copy(), raw.info.copy(), raw.info['ch_names'].copy(), raw._get_channel_positions().copy().tolist()
 
 
     @abstractmethod
     def _raw_from_data(self,
         data,
         data_info,
-        ch_coordinates
-        ) -> tuple[mne.io.Raw, list]:
-        pass
+        ch_coords
+        ) -> mne.io.Raw:
+        
+        raw = mne.io.RawArray(data, data_info)
+        raw._set_channel_positions(ch_coords, data_info['ch_names'])
+
+        return raw
 
 
     @abstractmethod
@@ -61,13 +65,17 @@ class Reref(ABC):
         ch_names = self._handle_empty(settings['ch_names_old'], settings['ch_names_new'], [])
         reref_types = settings['reref_types']
 
-        if len(ch_names) != len(reref_types):
+        equal_lengths, n_channels = CheckLengthsList.check_identical([ch_names, reref_types])
+        if not equal_lengths:
             raise Exception(f"Error when trying to assign channel rereferencing types.\nThere are {len(ch_names)} channels, but {len(reref_types)} rereferencing types.")
-        else:
-            len_entries = len(ch_names)
 
-        return {ch_names[i]: reref_types[i] for i in range(len_entries)}
+        return {ch_names[i]: reref_types[i] for i in range(n_channels)}
         
+
+    @abstractmethod
+    def _index_old_channels():
+        pass
+
 
     @abstractmethod
     def _set_data():
@@ -85,7 +93,7 @@ class Reref(ABC):
 
 
     @abstractmethod
-    def rereference():
+    def _rereference():
         pass
 
 
@@ -104,10 +112,12 @@ class RerefBipolar(Reref):
         self.raw = raw
         self._settings = settings
 
+        return self._rereference()
+
 
     def _data_from_raw(self):
 
-        self._data, self._data_info, self._ch_coordinates = super()._data_from_raw(self.raw)
+        self._data, self._data_info, self._ch_names, self._ch_coordinates = super()._data_from_raw(self.raw)
 
     
     def _raw_from_data(self):
@@ -137,21 +147,55 @@ class RerefBipolar(Reref):
         self.reref_types = super()._store_rereference_types(self._settings)
 
 
+    def _index_old_channels(self):
+        
+        self._ch_index = self._settings['ch_names_old'].copy()
+        for sublist_i, sublist in enumerate(self._settings['ch_names_old']):
+            for name_i, name in enumerate(sublist):
+                self._ch_index[sublist_i][name_i] = self._ch_names.index(name)
+
+
     def _set_data(self):
         
         if CheckLengthsList.check_equals_n(self._settings['ch_names_old'], 2):
             raise Exception(f"Error when bipolar rereferencing data.\nThis must involve two, and only two channels of data, but the rereferencing settings specify otherwise.")
         
-        for ch_i in len(self._n_channels):
+        self._new_data = []
+        for ch_i in range(self._n_channels):
+            self._new_data.append(self._data[self._ch_index[ch_i][0]] - self._data[self._ch_index[ch_i][1]])
 
 
+    def _set_coordinates(self):
+        
+        self._new_ch_coordinates = []
+        for ch_i in range(self._n_channels):
+            coords_set = False
+            if self._settings['ch_coords'] != []:
+                if self._settings['ch_coords'][ch_i] != []:
+                    if CheckLengthsList.check_equals_n(self._settings['ch_coords'], 3):
+                        raise Exception(f"Error when setting coordinates for the rereferenced data.\nThree, and only three coordinates (x, y, and z) must be present, but the rereferencing settings specify otherwise.")
+                    self._new_ch_coordinates.append(self._settings['ch_coords'][ch_i])
+                    coords_set = True
+            if coords_set == False:
+                self._new_ch_coordinates.append(
+                    np.around(np.mean([self._ch_coordinates[self._ch_index[ch_i][0]],
+                        self._ch_coordinates[self._ch_index[ch_i][1]]], axis=0), 2)
+                    )
 
-    def rereference(self
+
+    def _set_data_info(self):
+
+        self._new_data_info = mne.create_info(
+            self._settings['ch_names_new'], self._data_info['sfreq'], self._settings['ch_types']
+            )
+        
+
+    def _rereference(self
         ) -> tuple[mne.io.Raw, dict]:
 
-        self.data_from_raw()
-        self._set_data() # not implemented
-        self._set_coordinates() # not implemented
-        self._set_data_info() # not implemented
+        self._data_from_raw()
+        self._set_data()
+        self._set_coordinates()
+        self._set_data_info()
 
         return self._raw_from_data(), self._store_rereference_types()
--- coherence/coh_signal.py
+++ coherence/coh_signal.py
@@ -1,6 +1,6 @@
 import mne_bids
 import numpy as np
-from mne import read_annotations
+import mne
 from coh_rereference import RerefBipolar, RerefCAR, RerefNone
 
 
@@ -10,7 +10,7 @@ class Signal:
 
     def __init__(self,
         verbose: bool = True
-        ):
+        ) -> None:
 
         self._verbose = verbose
         self.processing_steps = []
@@ -26,7 +26,7 @@ class Signal:
     def set_coordinates(self,
         ch_coords: list,
         ch_names: list
-        ):
+        ) -> None:
 
         self.raw._set_channel_positions(ch_coords, ch_names)
 
@@ -42,7 +42,7 @@ class Signal:
 
     def load_raw(self,
         path_raw: mne_bids.BIDSPath
-        ):
+        ) -> None:
 
         self._path_raw = path_raw
         self.raw = mne_bids.read_raw_bids(bids_path=self.path_raw, verbose=False)
@@ -54,9 +54,9 @@ class Signal:
     
     def load_annotations(self,
         path_annots: str
-        ):
+        ) -> None:
 
-        self.raw.set_annotations(read_annotations(path_annots))
+        self.raw.set_annotations(mne.read_annotations(path_annots))
 
         if self._verbose:
             print(f"Applying annotations to the data from the filepath:\n{path_annots}.")
@@ -67,7 +67,7 @@ class Signal:
 
     def _pick_extra_info(self,
         ch_names: list
-        ):
+        ) -> None:
 
         for key in self.extra_info.keys():
             new_entry = {ch_name: self.extra_info[key][ch_name] for ch_name in ch_names}
@@ -76,7 +76,7 @@ class Signal:
     
     def pick_channels(self,
         ch_names: list
-        ):
+        ) -> None:
 
         self.raw.pick_channels(ch_names)
         self._pick_extra_info(ch_names)
@@ -89,7 +89,7 @@ class Signal:
     
     def bandpass_filter(self,
         freqs: list
-        ):
+        ) -> None:
 
         if len(freqs) != 2:
             raise Exception(f"Error when bandpass filtering the data.\nTwo frequencies are required, but {len(freqs)} are given.")
@@ -100,12 +100,12 @@ class Signal:
             print(f"Bandpass filtering the data.\nLow frequency: {freqs[0]} Hz. High frequency: {freqs[1]} Hz.")
 
         self._bandpass_filtered = True
-        self.processing_steps['bandpass_filter'] = freqs
+        self.processing_steps.append(['bandpass_filter', freqs])
 
     
     def notch_filter(self,
         line_freq: int
-        ):
+        ) -> None:
 
         freqs = np.arange(0, self.raw.info['lowpass']+1, line_freq)
         self.raw.notch_filter(freqs)
@@ -114,12 +114,12 @@ class Signal:
             print(f"Notch filtering the data with line noise frequency {line_freq} Hz at the following frequencies (Hz): {freqs}.")
 
         self._notch_filtered = True
-        self.processing_steps['notch_filter'] = freqs
+        self.processing_steps.append(['notch_filter', freqs])
 
 
     def resample(self,
         resample_freq: int
-        ):
+        ) -> None:
 
         self.raw.resample(resample_freq)
 
@@ -127,14 +127,57 @@ class Signal:
             print(f"Resampling the data at {resample_freq} Hz.")
 
         self._resampled = True
-        self.processing_steps['resample'] = resample_freq
+        self.processing_steps(['resample', resample_freq])
 
 
-    def rereferencing(self,
+    def _apply_rereference(self,
         reref_settings: dict
-        ):
+        ) -> list:
+
+        rerefed_raws = []
+        rerefed_types = []
+        for key in reref_settings:
+            if key == 'bipolar':
+                rerefed_data = RerefBipolar(self.raw, reref_settings)
+                rerefed_raws.append(rerefed_data[0])
+                rerefed_types.extend(rerefed_data[1])
+            elif key == 'CAR':
+                rerefed_data = RerefCAR(self.raw, reref_settings)
+                rerefed_raws.append(rerefed_data[0])
+                rerefed_types.extend(rerefed_data[1])
+            elif key == 'none':
+                rerefed_data = RerefNone(self.raw, reref_settings)
+                rerefed_raws.append(rerefed_data[0])
+                rerefed_types.extend(rerefed_data[1])
+            else:
+                raise Exception(f"Error when rereferencing data.\nThe rereferencing method {key} is not supported.")
+
+        return rerefed_raws
+
+
+    def _assign_rereferenced_data(self,
+        rerefed_data: list
+        ) -> None:
+
+        rerefed_raw = rerefed_data[0]
+        if len(rerefed_data) > 1:
+            rerefed_raw.add_channels([rerefed_data[1::]])
+        self.raw = rerefed_raw
+
+
+    def rereference(self,
+        reref_settings: dict
+        ) -> None:
         
-        self._rereferenced = True
+        rerefed_data, rerefed_types = self._apply_rereference(reref_settings)
+
+        if rerefed_data != []:
+            self._assign_rereferenced_data(rerefed_data)
+            self.processing_steps.append(['rereferencing', reref_settings])
+            self._rereferenced = True
+        else:
+            print("Warning when rereferencing data.\nYou are trying to rereference the data, but there are no recognised rereferencing methods in the settings you provide.\nHence, no rereferencing has taken place.")
+
 
 
 
