a216efd8fb0345d2c9c7defa2c572d9c06fd584f
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-03-12 19:49:37 +0100

    updated filepaths

--- analyses/data_processing.py
+++ analyses/data_processing.py
@@ -8,44 +8,55 @@ To be implemented:
 """
 
 
-
-
 import os
 import sys
 from pathlib import Path
 
 cd_path = Path(__file__).absolute().parent.parent
-sys.path.append(os.path.join(cd_path, 'coherence'))
+sys.path.append(os.path.join(cd_path, "coherence"))
 from coh_power_analysis import power_morlet_analysis
 from coh_preprocess_data import preprocessing
 
 
-
-
 ### Info about the data to analyse
-FOLDERPATH_DATA = 'D:\\Data'
-FOLDERPATH_EXTRAS = 'C:\\Users\\tomth\\OneDrive\\My Documents\\Data\\ICN'
-DATASET = 'BIDS_Berlin_ECOG_LFP'
-ANALYSIS = 'ECOG-LFP_coherence'
-SUBJECT = '001'
-SESSION = 'EphysMedOff01'
-TASK = 'Rest'
-ACQUISITION = 'StimOff'
-RUN = '01'
-
+FOLDERPATH_DATA = (
+    "C:\\Users\\User\\OneDrive - Charité - Universitätsmedizin Berlin\\PROJECT "
+    "ECOG-LFP Coherence\\Data"
+)
+FOLDERPATH_EXTRAS = "C:\\Users\\User\\OneDrive\\My Documents\\Data\\ICN"
+DATASET = "BIDS_Berlin_ECOG_LFP"
+ANALYSIS = "ECOG-LFP_coherence"
+SUBJECT = "001"
+SESSION = "EcogLfpMedOff01"
+TASK = "Rest"
+ACQUISITION = "StimOff"
+RUN = "1"
 
 
 if __name__ == "__main__":
 
     preprocessed = preprocessing(
-        FOLDERPATH_DATA, FOLDERPATH_EXTRAS, DATASET, ANALYSIS, SUBJECT, SESSION,
-        TASK, ACQUISITION, RUN
+        FOLDERPATH_DATA,
+        FOLDERPATH_EXTRAS,
+        DATASET,
+        ANALYSIS,
+        SUBJECT,
+        SESSION,
+        TASK,
+        ACQUISITION,
+        RUN,
     )
 
     power_morlet_analysis(
-        preprocessed, FOLDERPATH_EXTRAS, DATASET, ANALYSIS, SUBJECT, SESSION,
-        TASK, ACQUISITION, RUN
+        preprocessed,
+        FOLDERPATH_EXTRAS,
+        DATASET,
+        ANALYSIS,
+        SUBJECT,
+        SESSION,
+        TASK,
+        ACQUISITION,
+        RUN,
     )
-
 
     print("my name is jeff")
