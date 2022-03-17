604803c43d53ecc8acb4a8dbeaa2a696ac4ff9e8
tsbinns <t.s.binns@outlook.com>
2022-03-17 11:57:28 +0100

    updated matplotlib backend

--- analysis/annotate_data.py
+++ analysis/annotate_data.py
@@ -2,36 +2,34 @@ import sys
 import os
 from pathlib import Path
 from mne_bids import BIDSPath, read_raw_bids
-import matplotlib; matplotlib.use('TKAgg')
+import matplotlib; matplotlib.use('Qt5Agg')
 
 
 # Path info
 cd_path = Path(__file__).absolute().parent.parent
 sys.path.append(os.path.join(cd_path, 'coherence'))
 
-main_path = 'C:\\Users\\tomth\\Data\\BIDS_Beijing_ECOG_LFP\\rawdata'
-project_path = 'C:\\Users\\tomth\\OneDrive\\My Documents\\Work\\Courses\\Berlin\\ECN\\ICN\\Data\\BIDS_Beijing_ECOG_LFP\\projects\\coherence'
+main_path = 'C:\\Users\\tomth\\Data\\BIDS_Berlin_ECOG_LFP\\rawdata'
+project_path = 'C:\\Users\\tomth\\OneDrive\\My Documents\\Work\\Labs\\ICN\\Data\\BIDS_Berlin_ECOG_LFP\\projects\\coherence'
 
 from annotations import annotate, set_orig_time
 from helpers import filter_for_annotation
 
 # Setup
-sub = 'FOG011'
-ses = 'EphysMedOff01'
+sub = '010'
+ses = 'EphysMedOn01'
 task = 'Rest'
 acq = 'StimOff'
 run = '01'
 datatype = 'ieeg'
 
-channels = ["ECOG_R_1_SM_HH", "ECOG_R_2_SM_HH", "ECOG_R_3_SM_HH", "ECOG_R_4_SM_HH",
-                                             "ECOG_R_5_SM_HH", "ECOG_R_6_SM_HH", "ECOG_R_7_SM_HH", "ECOG_R_8_SM_HH",
-                                             "ECOG_R_9_SM_HH", "ECOG_R_10_SM_HH", "ECOG_R_11_SM_HH", "ECOG_R_12_SM_HH",
-                                             "ECOG_R_13_SM_HH", "ECOG_R_14_SM_HH", "ECOG_R_15_SM_HH", "ECOG_R_16_SM_HH",
-                                             "ECOG_R_17_SM_HH", "ECOG_R_18_SM_HH", "ECOG_R_19_SM_HH", "ECOG_R_20_SM_HH",
-                                             "ECOG_R_21_SM_HH", "ECOG_R_22_SM_HH", "ECOG_R_23_SM_HH", "ECOG_R_24_SM_HH",
-                                             "ECOG_R_25_SM_HH", "ECOG_R_26_SM_HH", "ECOG_R_27_SM_HH", "ECOG_R_28_SM_HH",
-                                             "ECOG_R_29_SM_HH", "ECOG_R_30_SM_HH",
-                                             "LFP_R_1_STN_PI", "LFP_R_4_STN_PI"]
+channels = [                                    "ECOG_R_2_SMC_AT",
+                                    "ECOG_R_3_SMC_AT",
+                                    "ECOG_R_4_SMC_AT",
+                                    "ECOG_R_5_SMC_AT",
+                                    "ECOG_R_6_SMC_AT",
+                                    "LFP_R_1_STN_MT",
+                                    "LFP_R_8_STN_MT"]
 filter_data = True
 orig_time = []
 
