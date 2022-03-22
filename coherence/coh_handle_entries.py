312645c21a1ae81be360daba269de67db6d72efb
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-03-22 23:30:39 +0100

    combine similar files

---
+++ coherence/coh_handle_entries.py
@@ -0,0 +1,480 @@
+"""Contains functions for handling entries within objects.
+
+METHODS
+-------
+check_lengths_dict_identical
+-   Checks whether the lengths of entries within a dictionary are identical.
+
+check_lengths_dict_equals_n
+-   Checks whether the lengths of entries within a dictionary is equal to a
+    given number.
+
+check_lengths_list_identical
+-   Checks whether the lengths of entries within a list are identical.
+
+check_lengths_list_equals_n
+-   Checks whether the lengths of entries within a list is equal to a given
+    number.
+
+check_duplicates_list
+-   Checks whether duplicates exist within an input list.
+
+check_matching_entries
+-   Checks whether the entries of objects match one another.
+
+check_master_entries_in_sublists
+-   Checks whether all values in a master list are present in a set of sublists.
+
+check_sublist_entries_in_master
+-   Checks whether all values in a set of sublusts are present in a master list.
+
+ordered_list_from_dict
+-   Creates a list from entries in a dictionary, sorted based on a given order.
+"""
+
+
+from itertools import chain
+from typing import Any, Optional, Union
+
+from coh_exceptions import DuplicateEntryError, EntryLengthError
+
+
+def _find_lengths_dict(
+    to_check: dict[Any, Any],
+    ignore_values: Optional[list[Any]],
+    ignore_keys: Optional[list[Any]],
+) -> list[int]:
+    """Finds the lengths of entries within a list.
+
+    PARAMETERS
+    ----------
+    to_check : list[Any]
+    -   The list for which the lengths of the entries should be checked.
+
+    ignore_values : list[Any] | None; default None
+    -   The values of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If None (default), no entries are ignored.
+
+    ignore_keys : list[Any] | None; default None
+    -   The keys of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If None (default), no entries are ignored.
+
+    RETURNS
+    -------
+    entry_lengths : list[int]
+    -   The lengths of entries in the list.
+    """
+
+    entry_lengths = []
+    for key, value in to_check.items():
+        if key not in ignore_keys or value not in ignore_values:
+            entry_lengths.append(len(value))
+
+    return entry_lengths
+
+
+def check_lengths_dict_identical(
+    to_check: dict[Any, Any],
+    ignore_values: Optional[list[Any]] = None,
+    ignore_keys: Optional[list[Any]] = None,
+) -> tuple[bool, Union[int, list[int]]]:
+    """Checks whether the lengths of entries in the input dictionary are
+    identical.
+
+    PARAMETERS
+    ----------
+    to_check : dict[Any, Any]
+    -   The dictionary for which the lengths of the entries should be checked.
+
+    ignore_values : list[Any]; optional, default []
+    -   The values of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If [] (default), no entries are ignored.
+
+    ignore_keys : list[Any]; optional, default []
+    -   The keys of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If [] (default), no entries are ignored.
+
+    RETURNS
+    -------
+    identical : bool
+    -   Whether or not the lengths of the entries are identical.
+
+    lengths : int | list
+    -   The length(s) of the entries. If the lengths are identical,
+        'lengths' is an int representing the length of all items.
+    -   If the lengths are not identical, 'lengths' is a list containing the
+        lengths of the individual entries (i.e. 'entry_lengths').
+    """
+
+    entry_lengths = _find_lengths_dict(
+        to_check=to_check, ignore_values=ignore_values, ignore_keys=ignore_keys
+    )
+
+    if entry_lengths.count(entry_lengths[0]) == len(entry_lengths):
+        identical = True
+        lengths = entry_lengths[0]
+    else:
+        identical = False
+        lengths = entry_lengths
+
+    return identical, lengths
+
+
+def check_lengths_dict_equals_n(
+    to_check: dict[Any, Any],
+    n: int,
+    ignore_values: Optional[list[Any]] = None,
+    ignore_keys: Optional[list[Any]] = None,
+) -> bool:
+    """Checks whether the lengths of entries in the input dictionary are equal
+    to a given number.
+
+    PARAMETERS
+    ----------
+    to_check : list[Any]
+    -   The list for which the lengths of the entries should be checked.
+
+    ignore_values : list[Any]; default []
+    -   The values of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If [] (default), no entries are ignored.
+
+    n : int
+    -   The integer which the lengths of the entries should be equal to.
+
+    RETURNS
+    -------
+    bool
+    -   Whether or not the lengths of the entries are equal to 'n'.
+    """
+
+    entry_lengths = _find_lengths_dict(
+        to_check=to_check, ignore_values=ignore_values, ignore_keys=ignore_keys
+    )
+
+    if entry_lengths.count(n) == len(entry_lengths):
+        all_n = True
+    else:
+        all_n = False
+
+    return all_n
+
+
+def _find_lengths_list(
+    to_check: list[Any], ignore_values: Optional[list[Any]]
+) -> list[int]:
+    """Finds the lengths of entries within a list.
+
+    PARAMETERS
+    ----------
+    to_check : list[Any]
+    -   The list for which the lengths of the entries should be checked.
+
+    ignore_values : list[Any] | None; default None
+    -   The values of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If None (default), no entries are ignored.
+
+    RETURNS
+    -------
+    entry_lengths : list[int]
+    -   The lengths of entries in the list.
+    """
+
+    if ignore_values is None:
+        ignore_values = []
+    entry_lengths = []
+    for value in to_check:
+        if value not in ignore_values:
+            entry_lengths.append(len(value))
+
+    return entry_lengths
+
+
+def check_lengths_list_identical(
+    to_check: list[Any], ignore_values: Optional[list[Any]] = None
+) -> tuple[bool, Union[int, list[int]]]:
+    """Checks whether the lengths of entries in the input dictionary are
+        identical.
+
+    PARAMETERS
+    ----------
+    to_check : list[Any]
+    -   The list for which the lengths of the entries should be checked.
+
+    ignore_values : list[Any] | None; default None
+    -   The values of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If None (default), no entries are ignored.
+
+    RETURNS
+    -------
+    identical : bool
+    -   Whether or not the lengths of the entries are identical.
+
+    lengths : int | list
+    -   The length(s) of the entries. If the lengths are identical,
+        'lengths' is an int representing the length of all items.
+    -   If the lengths are not identical, 'lengths' is a list containing the
+        lengths of the individual entries (i.e. 'entry_lengths').
+    """
+
+    entry_lengths = _find_lengths_list(
+        to_check=to_check, ignore_values=ignore_values
+    )
+
+    if entry_lengths.count(entry_lengths[0]) == len(entry_lengths):
+        identical = True
+        lengths = entry_lengths[0]
+    else:
+        identical = False
+        lengths = entry_lengths
+
+    return identical, lengths
+
+
+def check_lengths_list_equals_n(
+    to_check: list[Any], n: int, ignore_values: Optional[list[Any]] = None
+) -> bool:
+    """Checks whether the lengths of entries in the input dictionary are equal
+    to a given number.
+
+    PARAMETERS
+    ----------
+    to_check : list[Any]
+    -   The list for which the lengths of the entries should be checked.
+
+    ignore_values : list[Any] | None; default None
+    -   The values of entries within 'to_check' to ignore when checking the
+        lengths of entries.
+    -   If None (default), no entries are ignored.
+
+    n : int
+        -   The integer which the lengths of the entries should be equal to.
+
+    RETURNS
+    -------
+    bool
+    -   Whether or not the lengths of the entries are equal to 'n'.
+    """
+
+    entry_lengths = _find_lengths_list(
+        to_check=to_check, ignore_values=ignore_values
+    )
+
+    if entry_lengths.count(n) == len(entry_lengths):
+        all_n = True
+    else:
+        all_n = False
+
+    return all_n
+
+
+def check_duplicates_list(
+    values: list[Any],
+) -> tuple[bool, Optional[list[Any]]]:
+    """Checks whether duplicates exist within an input list.
+
+    PARAMETERS
+    ----------
+    values : list[Any]
+    -   The list of values whose entries should be checked for duplicates.
+
+    RETURNS
+    -------
+    duplicates : bool
+    -   Whether or not duplicates are present.
+
+    duplicate_values : list[Any] | None
+    -   The list of duplicates values, or None if no duplicates are present.
+    """
+
+    seen = set()
+    seen_add = seen.add
+    duplicate_values = list(
+        set(value for value in values if value in seen or seen_add(value))
+    )
+    if not duplicate_values:
+        duplicates = False
+        duplicate_values = None
+    else:
+        duplicates = True
+
+    return duplicates, duplicate_values
+
+
+def check_matching_entries(objects: list[Any]) -> bool:
+    """Checks whether the entries of objects match one another.
+
+    PARAMETERS
+    ----------
+    objects : list[Any]
+    -   The objects whose entries should be compared.
+
+    RETURNS
+    -------
+    matching : bool
+    -   If True, the entries of the objects match. If False, the entries do
+        not match.
+
+    RAISES
+    ------
+    EntryLengthError
+    -   Raised if the objects do not have equal lengths.
+    """
+
+    equal, length = check_lengths_dict_identical(objects)
+    if not equal:
+        raise EntryLengthError(
+            "Error when checking whether the entries of objects are "
+            f"identical:\nThe lengths of the objects ({length}) do not "
+            "match."
+        )
+
+    checking = True
+    matching = True
+    while checking or matching:
+        object_i = 1
+        for entry_i, base_value in enumerate(objects[0]):
+            for object_values in objects[1:]:
+                object_i += 1
+                if object_values[entry_i] != base_value:
+                    matching = False
+                    checking = False
+        checking = False
+
+    return matching
+
+
+def check_master_entries_in_sublists(
+    master_list: list[Any],
+    sublists: list[list[Any]],
+    allow_duplicates: bool = True,
+) -> tuple[bool, Optional[list[Any]]]:
+    """Checks whether all values in a master list are present in a set of
+    sublists.
+
+    PARAMETERS
+    ----------
+    master_list : list[Any]
+    -   A master list of values.
+
+    sublists : list[list[Any]]
+    -   A list of sublists of values.
+
+    allow_duplicates : bool; default True
+    -   Whether or not to allow duplicate values to be present in the sublists.
+
+    RETURNS
+    -------
+    all_present : bool
+    -   Whether all values in the master list were present in the sublists.
+
+    absent_entries : list[Any] | None
+    -   The entry/ies of the master list missing from the sublists. If no
+        entries are missing, this is None.
+    """
+
+    combined_sublists = list(chain(*sublists))
+
+    if not allow_duplicates:
+        duplicates, duplicate_entries = check_duplicates_list(combined_sublists)
+        if duplicates:
+            raise DuplicateEntryError(
+                "Error when checking the presence of master list entries "
+                f"within a sublist:\nThe entries {duplicate_entries} are "
+                "repeated within the sublists.\nTo ignore this error, set "
+                "'allow_duplicates' to True."
+            )
+
+    all_present = True
+    absent_entries = []
+    for entry in master_list:
+        if entry not in combined_sublists:
+            all_present = False
+            absent_entries.append(entry)
+    if not absent_entries:
+        absent_entries = None
+
+    return all_present, absent_entries
+
+
+def check_sublist_entries_in_master(
+    master_list: list[Any],
+    sublists: list[list[Any]],
+    allow_duplicates: bool = True,
+) -> tuple[bool, Optional[list[Any]]]:
+    """Checks whether all values in a set of sublists are present in a master
+    list.
+
+    PARAMETERS
+    ----------
+    master_list : list[Any]
+    -   A master list of values.
+
+    sublists : list[list[Any]]
+    -   A list of sublists of values.
+
+    allow_duplicates : bool; default True
+    -   Whether or not to allow duplicate values to be present in the sublists.
+
+    RETURNS
+    -------
+    all_present : bool
+    -   Whether all values in the sublists were present in the master list.
+
+    absent_entries : list[Any] | None
+    -   The entry/ies of the sublists missing from the master list. If no
+        entries are missing, this is None.
+    """
+
+    combined_sublists = list(chain(*sublists))
+
+    if not allow_duplicates:
+        duplicates, duplicate_entries = check_duplicates_list(combined_sublists)
+        if duplicates:
+            raise DuplicateEntryError(
+                "Error when checking the presence of master list entries "
+                f"within a sublist:\nThe entries {duplicate_entries} are "
+                "repeated within the sublists.\nTo ignore this error, set "
+                "'allow_duplicates' to True."
+            )
+
+    all_present = True
+    absent_entries = []
+    for entry in combined_sublists:
+        if entry not in master_list:
+            all_present = False
+            absent_entries.append(entry)
+    if not absent_entries:
+        absent_entries = None
+
+    return all_present, absent_entries
+
+
+def ordered_list_from_dict(
+    list_order: list[str], dict_to_order: dict[Any]
+) -> list[Any]:
+    """Creates a list from entries in a dictionary, sorted based on a given
+    order.
+
+    PARAMETERS
+    ----------
+    list_order : list[str]
+    -   The names of keys in the dictionary, in the order that
+        the values will occur in the list.
+
+    dict_to_order : dict[Any]
+    -   The dictionary whose entries will be added to the list.
+
+    RETURNS
+    -------
+    list[Any]
+    -   The ordered list.
+    """
+
+    return [dict_to_order[key] for key in list_order]
---
+++ coherence/coh_handle_files.py
@@ -0,0 +1,221 @@
+"""Functions for dealing with files.
+
+METHODS
+-------
+generate_raw_fpath
+-   Generates an mne_bids.BIDSPath object for loading an mne.io.Raw object.
+
+generate_sessionwise_fpath
+-   Generates a filepath for an object that corresponds to an individual
+    recording session based on the MNE data-storage filepath structure.
+
+generate_analysiswise_fpath
+-   Generates a filepath for an object that corresponds to a particular
+    analysis spanning multiple recordings sessions.
+
+check_ftype_present
+-   Checks whether a filetype is present in a filepath string based on the
+    presence of a period ('.').
+
+identify_ftype
+-   Finds what file type of a file is based on the filename extension.
+"""
+
+
+import os
+import mne_bids
+from coh_exceptions import MissingFileExtensionError
+
+
+def generate_raw_fpath(
+    folderpath: str,
+    dataset: str,
+    subject: str,
+    session: str,
+    task: str,
+    acquisition: str,
+    run: str,
+) -> mne_bids.BIDSPath:
+    """Generates an mne_bids.BIDSPath object for loading an mne.io.Raw object.
+
+    PARAMETERS
+    ----------
+    folderpath : str
+    -   The path of the folder where the datasets are located.
+
+    dataset : str
+    -   The name of the dataset within the folder given in 'folderpath'.
+
+    subject : str
+    -   The name of the subject for which the mne_bids.BIDSPath object should be
+        generated.
+
+    session : str
+    -   The name of the session for which the mne_bids.BIDSPath object should be
+        generated.
+
+    task : str
+    -   The name of the task for which the mne_bids.BIDSPath object should be
+        generated.
+
+    acquisition : str
+    -   The name of the acquisition mode for which the mne_bids.BIDSPath object
+        should be generated.
+
+    run : str
+    -   The name of the run for which the mne_bids.BIDSPath object should be
+        generated.
+
+    RETURNS
+    -------
+    mne_bids.BIDSPath
+    -   An mne_bids.BIDSPath object for loading an mne.io.Raw object.
+    """
+
+    return mne_bids.BIDSPath(
+        subject=subject,
+        session=session,
+        task=task,
+        acquisition=acquisition,
+        run=run,
+        root=os.path.join(folderpath, dataset, "rawdata"),
+    )
+
+
+def generate_sessionwise_fpath(
+    folderpath: str,
+    dataset: str,
+    subject: str,
+    session: str,
+    task: str,
+    acquisition: str,
+    run: str,
+    group_type: str,
+    filetype: str,
+) -> str:
+    """Generates a filepath for an object that corresponds to an individual
+    recording session based on the MNE data-storage filepath structure.
+
+    PARAMETERS
+    ----------
+    folderpath : str
+    -   The path of the folder where the datasets are located.
+
+    dataset : str
+    -   The name of the dataset folder within the folder given in 'folderpath'.
+
+    subject : str
+    -   The name of the subject for which the filepath should be generated.
+
+    session : str
+    -   The name of the session for which the filepath should be generated.
+
+    task : str
+    -   The name of the task for which the filepath should be generated.
+
+    acquisition : str
+    -   The name of the acquisition mode for which the filepath should be
+        generated.
+
+    run : str
+    -   The name of the run for which the filepath should be generated.
+
+    group_type : str
+    -   The name of the group of files for which the filepath should be
+        generate, e.g. 'annotations', 'settings'.
+
+    filetype : str
+    -   The file extension, prefixed with a period, e.g. '.json', '.csv'.
+
+    RETURNS
+    -------
+    str
+    -   The filepath of the object.
+    """
+
+    subfolders = f"{dataset}\\sub-{subject}\\ses-{session}"
+    filename = (
+        f"sub-{subject}_ses-{session}_task-{task}_acq-{acquisition}_run-{run}_"
+        f"{group_type}{filetype}"
+    )
+
+    return os.path.join(folderpath, subfolders, filename)
+
+
+def generate_analysiswise_fpath(
+    folderpath: str, analysis_name: str, filetype: str
+) -> str:
+    """Generates a filepath for an object that corresponds to a particular
+    analysis spanning multiple recordings sessions.
+
+    PARAMETERS
+    ----------
+    folderpath : str
+    -   The path of the folder where the datasets are located.
+
+    analysis_name : str
+    -   The name of the analysis folder within the folder given in
+        "'folderpath'/settings".
+
+    filetype : str
+    -   The file extension, prefixed with a period, e.g. '.json', '.csv'.
+
+    RETURNS
+    -------
+    str
+    -   The filepath of the object.
+    """
+
+    return os.path.join(folderpath, analysis_name + filetype)
+
+
+def check_ftype_present(fpath: str) -> bool:
+    """Checks whether a filetype is present in a filepath string based on the
+    presence of a period ('.').
+
+    PARAMETERS
+    ----------
+    fpath : str
+    -   The filepath, including the filename.
+
+    RETURNS
+    -------
+    ftype_present : bool
+    -   Whether or not a filetype is present.
+    """
+
+    if "." in fpath:
+        ftype_present = True
+    else:
+        ftype_present = False
+
+    return ftype_present
+
+
+def identify_ftype(fpath: str) -> str:
+    """Finds what file type of a file is based on the filename extension.
+
+    PARAMETERS
+    ----------
+    fpath : str
+    -   The filepath, including the filename and extension.
+
+    RETURNS
+    -------
+    str
+    -   The file type.
+
+    RAISES
+    ------
+    MissingFileExtensionError
+    -   Raised if 'fpath' is missing the filetype extension.
+    """
+
+    if not check_ftype_present(fpath):
+        raise MissingFileExtensionError(
+            "Error when determining the filetype:\nNo filetype can be found in "
+            f"the filepath '{fpath}'.\nFilepaths should be in the format "
+            "'filename.filetype'."
+        )
+
+    return fpath[fpath.rfind(".") + 1 :]
