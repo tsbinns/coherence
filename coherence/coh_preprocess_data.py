6852a6034ee994e3d8333b573ab7d8770267f692
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-04-07 20:04:12 +0200

    added preprocessing for annotating

--- coherence/coh_preprocess_data.py
+++ coherence/coh_preprocess_data.py
@@ -3,7 +3,12 @@
 METHODS
 -------
 preprocessing
--   Loads an mne.io.Raw object, preprocesses it, and epochs it.
+-   Loads an mne.io.Raw object, preprocesses it, and epochs it in preparation
+    for analysis.
+
+preprocessing_for_annotations
+-   Loads an mne.io.Raw object and preprocesses it in preparation for
+    annotating.
 """
 
 from coh_handle_files import (
@@ -143,5 +148,92 @@ def preprocessing(
     ## Adds metadata about the preprocessed data
     metadata = ExtractMetadata(data_settings).metadata
     signal.add_metadata(metadata)
+
+    return signal
+
+
+def preprocessing_for_annotating(
+    folderpath_data: str,
+    folderpath_extras: str,
+    dataset: str,
+    analysis: str,
+    subject: str,
+    session: str,
+    task: str,
+    acquisition: str,
+    run: str,
+) -> Signal:
+    """Loads an mne.io.Raw object and preprocesses it in preparation for adding
+    annotations.
+
+    PARAMETERS
+    ----------
+    folderpath_data : str
+    -   The folderpath to the location of the datasets.
+
+    folderpath_extras : str
+    -   The folderpath to the location of the datasets' 'extras', e.g. the
+        annotations, processing settings, etc...
+
+    dataset : str
+    -   The name of the dataset folder found in 'folderpath_data'.
+
+    analysis : str
+    -   The name of the analysis folder within "'folderpath_extras'/settings".
+
+    subject : str
+    -   The name of the subject whose data will be analysed.
+
+    session : str
+    -   The name of the session for which the data will be analysed.
+
+    task : str
+    -   The name of the task for which the data will be analysed.
+
+    acquisition : str
+    -   The name of the acquisition mode for which the data will be analysed.
+
+    run : str
+    -   The name of the run for which the data will be analysed.
+
+    RETURNS
+    -------
+    signal : Signal
+    -   The preprocessed and epoched data.
+    """
+
+    ### Analysis setup
+    ## Gets the relevant filepaths
+    analysis_settings_fpath = generate_analysiswise_fpath(
+        folderpath_extras + "\\settings", analysis, ".json"
+    )
+    data_settings_fpath = generate_sessionwise_fpath(
+        folderpath_extras,
+        dataset,
+        subject,
+        session,
+        task,
+        acquisition,
+        run,
+        "settings",
+        ".json",
+    )
+    raw_fpath = generate_raw_fpath(
+        folderpath_data, dataset, subject, session, task, acquisition, run
+    )
+
+    ## Loads the analysis settings
+    analysis_settings = load_file(fpath=analysis_settings_fpath)
+    analysis_settings = analysis_settings["preprocessing"]
+    data_settings = load_file(fpath=data_settings_fpath)
+
+    ### Data Pre-processing
+    signal = Signal()
+    signal.load_raw(raw_fpath)
+    signal.pick_channels(data_settings["ch_names"])
+    signal.notch_filter(analysis_settings["line_noise"])
+    signal.bandpass_filter(
+        analysis_settings["lowpass"], analysis_settings["highpass"]
+    )
 
     return signal
