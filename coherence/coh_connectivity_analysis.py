c4df0cc530104e27ce76a003827360df7d4cf032
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-04-13 17:26:33 +0200

    added connectivity analysis

--- coherence/coh_connectivity_analysis.py
+++ coherence/coh_connectivity_analysis.py
@@ -6,9 +6,7 @@ coherence_analysis
 -   Analyses coherence results.
 """
 
-
-from coh_handle_files import generate_results_fpath, load_file
-from coh_postprocess import PostProcess
+from coh_postprocess import load_results_of_types
 
 
 def coherence_analysis(
@@ -39,44 +37,11 @@ def coherence_analysis(
     identical_entries = ["freqs"]
     discard_entries = ["samp_freq", "subject_info", "coherence_dimensions"]
 
-    first_type = True
-    for type_i, result_type in enumerate(result_types):
-        if type_i > 0:
-            first_type = False
-        first_result = True
-        for result_info in to_analyse.values():
-            result_fpath = generate_results_fpath(
-                folderpath=results_folderpath,
-                subject=result_info["sub"],
-                session=result_info["ses"],
-                task=result_info["task"],
-                acquisition=result_info["acq"],
-                run=result_info["run"],
-                result_type=result_type,
-                filetype=".json",
-            )
-            result = load_file(fpath=result_fpath)
-            if first_type:
-                if first_result:
-                    results = PostProcess(
-                        results=result,
-                        extract_from_dicts=extract_from_dicts,
-                        identical_entries=identical_entries,
-                        discard_entries=discard_entries,
-                    )
-                    first_result = False
-                else:
-                    results.append(
-                        results=result,
-                        extract_from_dicts=extract_from_dicts,
-                        identical_entries=identical_entries,
-                        discard_entries=discard_entries,
-                    )
-            else:
-                results.merge(
-                    results=result,
-                    extract_from_dicts=extract_from_dicts,
-                    identical_entries=identical_entries,
-                    discard_entries=discard_entries,
-                    allow_missing=False,
-                )
+    results = load_results_of_types(
+        results_folderpath=results_folderpath,
+        to_analyse=to_analyse,
+        result_types=result_types,
+        extract_from_dicts=extract_from_dicts,
+        identical_entries=identical_entries,
+        discard_entries=discard_entries,
+    )
