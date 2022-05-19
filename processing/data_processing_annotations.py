26dbdda833fbfeb6bbb7830b1e4636c0a05f9aba
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-05-19 17:56:11 +0200

    refactoring

--- processing/data_processing_annotations.py
+++ processing/data_processing_annotations.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 cd_path = Path(__file__).absolute().parent.parent
 sys.path.append(os.path.join(cd_path, "coherence"))
-from coh_preprocess_data import preprocessing
+from coh_preprocess_data import preprocessing_for_annotations
 from coh_view_data import annotate_data
 
 
@@ -16,12 +16,12 @@ FOLDERPATH_DATA = (
     "C:\\Users\\User\\OneDrive - Charité - Universitätsmedizin Berlin\\"
     "PROJECT ECOG-LFP Coherence\\Data"
 )
-FOLDERPATH_PROCESSING = (
-    "C:\\Users\\User\\OneDrive\\My Documents\\Data\\ICN\\Processing"
+FOLDERPATH_PREPROCESSING = (
+    "C:\\Users\\User\\OneDrive\\My Documents\\Data\\ICN\\Preprocessing"
 )
 DATASET = "BIDS_Berlin_ECOG_LFP"
-ANALYSIS = "ECOG-LFP_coherence-annotations_processing"
-SUBJECT = "001"
+ANALYSIS = "for_annotations"
+SUBJECT = "004"
 SESSION = "EcogLfpMedOn02"
 TASK = "Rest"
 ACQUISITION = "StimOff"
@@ -30,9 +30,9 @@ RUN = "1"
 
 if __name__ == "__main__":
 
-    preprocessed = preprocessing(
+    preprocessed = preprocessing_for_annotations(
         FOLDERPATH_DATA,
-        FOLDERPATH_PROCESSING,
+        FOLDERPATH_PREPROCESSING,
         DATASET,
         ANALYSIS,
         SUBJECT,
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     annotate_data(
         preprocessed,
-        FOLDERPATH_PROCESSING,
+        FOLDERPATH_PREPROCESSING,
         DATASET,
         SUBJECT,
         SESSION,
