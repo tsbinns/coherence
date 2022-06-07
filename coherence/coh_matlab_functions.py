dc1501bbb81b93e4e1f8332a65fbeebf8e510961
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-06-07 23:23:42 +0200

    add methods

--- coherence/coh_matlab_functions.py
+++ coherence/coh_matlab_functions.py
@@ -6,26 +6,37 @@ mrdivide
 -   Performs a matrix right division, equivalent to the 'mrdivide' function or
     '/' operator in MATLAB.
 
+mldivide
+-   Performs a matrix left division, equivalent to the 'mrdivide' function or \
+    operator in MATLAB.
+
+reshape
 -   Equivalent to the MATLAB 'reshape' function, whereby the elements from the
     first axis onwards are taken in some order for the reshaping (i.e. from axis
     0 to n).
+
+kron
+-   Equivalent to the MATLAB 'kron' function, in which the Kronecker product is
+    calculated.
 """
 
+from typing import Union
 from numpy.typing import NDArray
 import numpy as np
+import scipy as sp
 
 
-def mrdivide(numerator: NDArray, denominator: NDArray) -> NDArray:
+def mrdivide(A: NDArray, B: NDArray) -> NDArray:
     """Performs a matrix right division, equivalent to the 'mrdivide' function
     or '/' operator in MATLAB.
 
     PARAMETERS
     ----------
-    numerator : numpy array
-    -   An n x m matrix that will be the numerator in the division.
+    A : numpy array
+    -   An n x m matrix on the left side of the division.
 
-    denominator : numpy array
-    -   An n x m matrix that will be the denominator in the division.
+    B : numpy array
+    -   An n x m matrix on the right side of the division.
 
     RETURNS
     -------
@@ -33,10 +44,31 @@ def mrdivide(numerator: NDArray, denominator: NDArray) -> NDArray:
     -   The result of the matrix right division.
     """
 
-    return np.linalg.solve(denominator.conj().T, numerator.conj().T).conj().T
+    return np.linalg.solve(B.conj().T, A.conj().T).conj().T
 
 
-def reshape(array: NDArray, dims: tuple[int]) -> NDArray:
+def mldivide(A: NDArray, B: NDArray) -> NDArray:
+    """Performs a matrix left division, equivalent to the 'mldivide' function or
+    \ operator in MATLAB.
+
+    PARAMETERS
+    ----------
+    A : numpy array
+    -   An n x m matrix on the left side of the division.
+
+    B : numpy array
+    -   An n x m matrix on the right side of the division.
+
+    RETURNS
+    -------
+    numpy array
+    -   The result of the matrix left division.
+    """
+
+    return np.linalg.lstsq(A, B, rcond=None)[0]
+
+
+def reshape(array: NDArray, dims: Union[int, tuple[int]]) -> NDArray:
     """Equivalent to the MATLAB 'reshape' function, whereby the elements from
     the first axis onwards are taken in some order for the reshaping (i.e. from
     axis 0 to n).
@@ -49,7 +81,7 @@ def reshape(array: NDArray, dims: tuple[int]) -> NDArray:
     array : numpy array
     -   Array which will be reshaped.
 
-    dims : tuple[int]
+    dims : int | tuple[int]
     -   The dimensions of the reshaped array.
 
     RETURNS
@@ -63,3 +95,35 @@ def reshape(array: NDArray, dims: tuple[int]) -> NDArray:
     """
 
     return np.reshape(array, dims, order="F")
+
+
+def kron(A: NDArray, B: NDArray) -> NDArray:
+    """Equivalent to the MATLAB 'kron' function, in which the Kronecker product
+    is calculated.
+
+    PARAMETERS
+    ----------
+    A : numpy array
+    -   A matrix.
+
+    B : numpy array
+    -   A matrix.
+
+    RETURNS
+    -------
+    K : numpy array
+    -   The Kronecker product of 'A' and 'B'.
+
+    NOTES
+    -----
+    -   If the matrices are not sparse, the numpy 'kron' function is used. If
+        either of the matrices are sparse, the scipy.sparse function 'kron' is
+        used.
+    """
+
+    if not sp.sparse.issparse(A) and not sp.sparse.issparse(B):
+        K = np.kron(A, B)
+    else:
+        K = sp.sparse.kron(A, B)
+
+    return K
