6e244c8e861456e50965a4229dacc0e82d0316eb
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-06-08 20:22:44 +0200

    added GC calculation

--- coherence/coh_connectivity_computations.py
+++ coherence/coh_connectivity_computations.py
@@ -17,11 +17,19 @@ multivariate_connectivity_compute_E
     groups A and B.
 """
 
-from numpy.typing import NDArray
+from typing import Union
 import numpy as np
+from numpy.typing import NDArray
+from coh_matlab_functions import reshape
 from coh_signal_processing_computations import (
+    autocovariance_to_full_var,
+    csd_to_autocovariance,
     multivariate_connectivity_compute_e,
+    ss_params_to_gc,
+    var_to_ss_params,
 )
+
+from scipy.io import loadmat
 
 
 def multivariate_connectivity(
@@ -64,7 +72,6 @@ def multivariate_connectivity(
     NotImplementedError
     -   Raised if the requested method is not supported.
     """
-
     supported_methods = ["mim", "mic"]
     if method not in supported_methods:
         raise NotImplementedError(
@@ -126,15 +133,14 @@ def multivariate_interaction_measure(
     -   Translated into Python by Thomas Samuel Binns (@tsbinns) from MATLAB
         code provided by Franziska Pellegrini of Stefan Haufe's research group.
     """
-
-    if len(np.shape(data)) != 3:
+    if len(data.shape) != 3:
         raise ValueError(
             "Error when computing MIC:\nThe data must be a three-dimensional "
             "array containing connectivity values across frequencies, but the "
-            f"data has {len(np.shape(data))} dimensions."
+            f"data has {len(data.shape)} dimensions."
         )
     n_signals = n_group_a + n_group_b
-    if (n_signals, n_signals) != np.shape(data)[0:2]:
+    if (n_signals, n_signals) != data.shape[0:2]:
         raise ValueError(
             "Error when calculating the multivariate interaction measure:\nThe "
             f"data for each frequency must be a [{n_signals} x {n_signals}] "
@@ -144,7 +150,7 @@ def multivariate_interaction_measure(
             "matrix."
         )
 
-    n_freqs = np.shape(data)[2]
+    n_freqs = data.shape[2]
     mim = np.empty(n_freqs)
     for freq_i in range(n_freqs):
         # Equations 2-4
@@ -198,29 +204,28 @@ def max_imaginary_coherence(
     -   Translated into Python by Thomas Samuel Binns (@tsbinns) from MATLAB
         code provided by Franziska Pellegrini of Stefan Haufe's research group.
     """
-
-    if len(np.shape(data)) != 3:
+    if len(data.shape) != 3:
         raise ValueError(
             "Error when computing MIC:\nThe data must be a three-dimensional "
             "array containing connectivity values across frequencies, but the "
-            f"data has {len(np.shape(data))} dimensions."
+            f"data has {len(data.shape)} dimensions."
         )
     n_signals = n_group_a + n_group_b
-    if (n_signals, n_signals) != np.shape(data)[0:2]:
+    if (n_signals, n_signals) != data.shape[0:2]:
         raise ValueError(
             "Error when calculating the multivariate interaction measure:\nThe "
             f"data for each frequency must be a [{n_signals} x {n_signals}] "
             "square matrix containing all connectivities between the "
             f"{n_group_a} signals in group A and the {n_group_b} signals in "
-            f"group B, but it is a [{np.shape(data)[0]} x {np.shape(data)[1]}] "
+            f"group B, but it is a [{data.shape[0]} x {data.shape[1]}] "
             "matrix."
         )
 
-    n_freqs = np.shape(data)[2]
+    n_freqs = data.shape[2]
     mic = np.empty(n_freqs)
     for freq_i in range(n_freqs):
         # Equations 2-4
-        E = multivariate_connectivity_compute_E(
+        E = multivariate_connectivity_compute_e(
             data=data[:, :, freq_i], n_group_a=n_group_a
         )
 
@@ -238,3 +243,201 @@ def max_imaginary_coherence(
         )
 
     return mic
+
+
+def granger_causality(
+    csd: NDArray,
+    freqs: list[Union[int, float]],
+    method: str,
+    seeds: list[list[Union[int, float]]],
+    targets: list[list[Union[int, float]]],
+    n_lags: int = 20,
+) -> NDArray:
+    """Computes frequency-domain Granger causality (GC) between each set of
+    seeds and targets.
+
+    PAREMETERS
+    ----------
+    csd : numpy array
+    -   Matrix containing the cross-spectral density between signals, with
+        dimensions [n_signals x n_signals x n_frequencies].
+
+    freqs : list[int | float]
+    -   Frequencies in 'csd'.
+
+    method : str
+    -   Which form of GC to compute.
+    -   Supported inputs are: "gc" for GC from seeds to targets; "net_gc" for
+        net GC, i.e. GC from seeds to targets minus GC from targets to seeds;
+        "trgc" for time-reversed GC (TRGC) from seeds to targets; and "net_trgc"
+        for net TRGC, i.e. TRGC from seeds to targets minus TRGC from targets to
+        seeds.
+
+    seeds : list[list[int | float]]
+    -   Indices of signals in 'csd' to treat as seeds. Should be a list of
+        sublists, where each sublist contains the signal indices that will be
+        treated as a group of seeds.
+    -   The number of sublists must match the number of sublists in 'targets'.
+
+    targets : list[list[int | float]]
+    -   Indices of signals in 'csd' to treat as targets. Should be a list of
+        sublists, where each sublist contains the signal indices that will be
+        treated as a group of targets.
+    -   The number of sublists must match the number of sublists in 'seeds'.
+
+    n_lags : int; default 20
+    -   Number of lags to use when computing the autocovariance sequence from
+        the cross-spectra.
+
+    RETURNS
+    -------
+    numpy array
+    -   Granger causality values in a matrix with dimensions [n_nodes x
+        n_frequencies], where the nodes correspond to seed-target pairs.
+
+    NOTES
+    -----
+    -   Net TRGC is the recommended method for maximum robustness.
+    -   Each group of seeds and targets cannot contain the same indices.
+    """
+    autocov = csd_to_autocovariance(csd, n_lags)
+
+    return gc_computation(
+        autocov=autocov,
+        freqs=freqs,
+        method=method,
+        seeds=seeds,
+        targets=targets,
+    )
+
+
+def gc_computation(
+    autocov: NDArray,
+    freqs: list[Union[int, float]],
+    method: str,
+    seeds: list[list[Union[int, float]]],
+    targets: list[list[Union[int, float]]],
+) -> NDArray:
+    """Computes frequency-domain Granger causality.
+
+    PARAMETERS
+    ----------
+    autocov : numpy array
+    -   An autocovariance sequence with dimensions [n_signals x n_signals x
+        n_lags + 1].
+
+    freqs : list[int | float]
+    -   Frequencies pf the data being analysed.
+
+    method : str
+    -   Which form of GC to compute.
+    -   Supported inputs are: "gc" for GC from seeds to targets; "net_gc" for
+        net GC, i.e. GC from seeds to targets minus GC from targets to seeds;
+        "trgc" for time-reversed GC (TRGC) from seeds to targets; and "net_trgc"
+        for net TRGC, i.e. TRGC from seeds to targets minus TRGC from targets to
+        seeds.
+
+    seeds : list[list[int | float]]
+    -   Indices of signals in 'csd' to treat as seeds. Should be a list of
+        sublists, where each sublist contains the signal indices that will be
+        treated as a group of seeds.
+    -   The number of sublists must match the number of sublists in 'targets'.
+
+    targets : list[list[int | float]]
+    -   Indices of signals in 'csd' to treat as targets. Should be a list of
+        sublists, where each sublist contains the signal indices that will be
+        treated as a group of targets.
+    -   The number of sublists must match the number of sublists in 'seeds'.
+
+    RETURNS
+    -------
+    gc_vals : numpy array
+    -   Granger causality values in a matrix with dimensions [n_nodes x
+        n_frequencies], where the nodes correspond to seed-target pairs.
+
+    RAISES
+    ------
+    NotImplementedError
+    -   Raised if 'method' is not supported.
+    ValueError
+    -   Raised if the number of seed and target groups do not match.
+    """
+    supported_methods = ["gc", "net_gc", "trgc", "net_trgc"]
+    if method not in supported_methods:
+        raise NotImplementedError(
+            f"The method '{method}' for computing frequency-domain Granger "
+            "causality is not recognised. Supported methods are "
+            f"{supported_methods}."
+        )
+    if len(seeds) != len(targets):
+        raise ValueError(
+            f"The number of seed and target groups ({len(seeds)} and "
+            f"{len(targets)}, respectively) do not match."
+        )
+
+    gc_vals = np.zeros((len(seeds), len(freqs)))
+    node_i = 0
+    for seed_idcs, target_idcs in zip(seeds, targets):
+        node_idcs = [*seed_idcs, *target_idcs]
+        seed_idcs_new = np.arange(len(seed_idcs)).tolist()
+        target_idcs_new = np.arange(
+            start=len(seed_idcs), stop=len(node_idcs)
+        ).tolist()
+        var_coeffs, residuals_cov = autocovariance_to_full_var(
+            autocov[np.ix_(node_idcs, node_idcs, np.arange(autocov.shape[2]))],
+            enforce_posdef_residuals_cov=True,
+        )
+        var_coeffs_2d = reshape(
+            var_coeffs,
+            (var_coeffs.shape[0], var_coeffs.shape[0] * var_coeffs.shape[2]),
+        )
+        A, K = var_to_ss_params(AF=var_coeffs_2d, V=residuals_cov)
+        gc_vals[node_i, :] = ss_params_to_gc(
+            A=A,
+            C=var_coeffs_2d,
+            K=K,
+            V=residuals_cov,
+            freqs=freqs,
+            seeds=seed_idcs_new,
+            targets=target_idcs_new,
+        )
+        if "net" in method:
+            gc_vals[node_i, :] -= ss_params_to_gc(
+                A=A,
+                C=var_coeffs_2d,
+                K=K,
+                V=residuals_cov,
+                freqs=freqs,
+                seeds=target_idcs_new,
+                targets=seed_idcs_new,
+            )
+        node_i += 1
+
+    if "trgc" in method:
+        if method == "trgc":
+            tr_method = "gc"
+        elif method == "net_trgc":
+            tr_method = "net_gc"
+        gc_vals -= (
+            gc_computation(
+                autocov=np.transpose(autocov, (1, 0, 2)),
+                freqs=freqs,
+                method=tr_method,
+                seeds=seeds,
+                targets=targets,
+            )
+        )
+
+    return gc_vals
+
+
+csd = loadmat("coherence\\csd.mat")["CS"]
+gc_vals = granger_causality(
+    csd=csd,
+    freqs=np.arange(csd.shape[2]),
+    method="net_trgc",
+    seeds=[[0, 1, 2] for n in range(4)],
+    targets=[[3], [4], [5], [6]],
+    n_lags=20,
+)
+print("jeff")
