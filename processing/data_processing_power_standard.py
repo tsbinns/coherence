c2f88e385137a9b635c8698eeca916a3f6523192
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-06-30 19:04:57 +0200

    renamed file

---
+++ processing/data_processing_power_standard.py
@@ -0,0 +1,57 @@
+"""Processes ECoG and LFP data to generate power spectral densities"""
+
+
+import os
+import sys
+from pathlib import Path
+
+cd_path = Path(__file__).absolute().parent.parent
+sys.path.append(os.path.join(cd_path, "coherence"))
+from coh_power_processing import standard_power_analysis
+from coh_loading import load_preprocessed_dict
+
+
+### Info about the data to analyse
+FOLDERPATH_PREPROCESSING = (
+    "\\\\?\\C:\\Users\\User\\OneDrive - Charité - Universitätsmedizin Berlin\\"
+    "PROJECT ECOG-LFP Coherence\\Analysis\\Preprocessing"
+)
+FOLDERPATH_PROCESSING = (
+    "\\\\?\\C:\\Users\\User\\OneDrive - Charité - Universitätsmedizin Berlin\\"
+    "PROJECT ECOG-LFP Coherence\\Analysis\\Processing"
+)
+DATASET = "BIDS_Berlin_ECOG_LFP"
+PREPROCESSING = "preprocessed-with_notch_filter"
+ANALYSIS = "pow_morlet"
+SUBJECT = "006"
+SESSION = "EcogLfpMedOff01"
+TASK = "Rest"
+ACQUISITION = "StimOff"
+RUN = "1"
+
+
+if __name__ == "__main__":
+
+    preprocessed = load_preprocessed_dict(
+        folderpath_preprocessing=FOLDERPATH_PREPROCESSING,
+        dataset=DATASET,
+        preprocessing=PREPROCESSING,
+        subject=SUBJECT,
+        session=SESSION,
+        task=TASK,
+        acquisition=ACQUISITION,
+        run=RUN,
+    )
+
+    standard_power_analysis(
+        preprocessed,
+        FOLDERPATH_PROCESSING,
+        DATASET,
+        ANALYSIS,
+        SUBJECT,
+        SESSION,
+        TASK,
+        ACQUISITION,
+        RUN,
+        save=True,
+    )
